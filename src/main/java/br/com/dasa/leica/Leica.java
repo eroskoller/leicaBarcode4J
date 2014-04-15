ff4ce52d90ad519e392ae87332d7cd664a3140ea
eros <eroskoller@gmail.com>
2014-04-15 09:48:12 -0300

    Done

---
+++ src/main/java/br/com/dasa/leica/Leica.java
@@ -0,0 +1,293 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.dasa.leica;
+
+
+import br.com.img.generator.Code128Util;
+import com.google.zxing.WriterException;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import static java.awt.print.Printable.NO_SUCH_PAGE;
+import static java.awt.print.Printable.PAGE_EXISTS;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.print.DocFlavor;
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.standard.Copies;
+import javax.print.attribute.standard.MediaPrintableArea;
+
+/**
+ *
+ * @author eros
+ */
+public class Leica implements Printable {
+
+    public static void main(String[] args) {
+
+        System.out.println("Inside class Leica ");
+        int ic = 0;
+        int ia = 8;
+        int tc = 5;
+        int ta = 0;
+        int fc = 70;
+        int fa = 20;
+        String codigo = null;
+        String saida = null;
+        String impressora = null;
+        String tipoimpressora = null;
+        int porta = 0;
+        String currentPath = Leica.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-ic":
+                    ic = Integer.parseInt(args[++i]);
+                    break;
+                case "-ia":
+                    ia = Integer.parseInt(args[++i]);
+                    break;
+                case "-tc":
+                    tc = Integer.parseInt(args[++i]);
+                    break;
+                case "-ta":
+                    ta = Integer.parseInt(args[++i]);
+                    break;
+                case "-fc":
+                    fc = Integer.parseInt(args[++i]);
+                    break;
+                case "-fa":
+                    fa = Integer.parseInt(args[++i]);
+                    break;
+                case "-codigo":
+                    codigo = args[++i];
+                    break;
+                case "-saida":
+                    saida = args[++i];
+                    break;
+                case "-impressora":
+                    impressora = args[++i];
+                    break;
+                case "-tipoimpressora":
+                    tipoimpressora = args[++i];
+                    break;
+                case "-impressoras":
+                    impressoras(null, tipoimpressora);
+                    System.exit(0);
+                    break;
+                case "-porta":
+                    porta = Integer.parseInt(args[++i]);
+                    break;
+            }
+        }
+        Leica programa = new Leica();
+        PrinterJob job = null;
+        try {
+            job = programa.obtemJob(tipoimpressora, impressora);
+        } catch (PrinterException ex) {
+            Logger.getLogger(Leica.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(9);
+        }
+        if (porta != 0) {
+            try (ServerSocket serverSocket = new ServerSocket(porta);
+                    Socket clientSocket = serverSocket.accept();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+                while ((codigo = in.readLine()) != null && codigo.length()>0) {
+                    codigo = codigo.trim().replaceAll("\\D", "");
+//                    System.out.println("codigo: " + codigo);
+                    if (codigo.length() == 0) {
+                        continue;
+                    }
+//                    System.out.println("codigo.trim() : " + codigo.trim()+"     currentPath : "+currentPath);
+                    programa.principal(fc, fa, tc, ta, job, codigo, saida, ic, ia, currentPath);
+                    new File(currentPath+codigo+".png").delete();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(Leica.class.getName()).log(Level.SEVERE, null, ex);
+                System.exit(10);
+            }
+        } else {
+            programa.principal(fc, fa, tc, ta, job, codigo, saida, ic, ia, currentPath);
+        }
+    }
+
+    private BufferedImage imagem = null;
+    private int tc = 0;
+    private int ta = 0;
+    private int fc = 0;
+    private int fa = 0;
+
+    private void principal(int fc, int fa, int tc, int ta, PrinterJob job, String codigo, String saida, int ic, int ia, String path) {
+        this.tc = tc;
+        this.ta = ta;
+        this.fc = fc;
+        this.fa = fa;
+        byte[] buffer = null;
+        try {
+//            System.out.println("path, codigo, ic, ia : "+path+ codigo);
+            buffer = geraPNG(path, codigo, ic, ia);
+//            System.out.println("buffer: " + buffer);
+        } catch (WriterException ex) {
+            Logger.getLogger(Leica.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(1);
+        } catch (IOException ex) {
+            Logger.getLogger(Leica.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(2);
+        }
+
+        try {
+//            imagem = ImageIO.read(new ByteArrayInputStream(buffer));
+
+//            File f = new File
+            Image imageReal = ImageIO.read(new File(path + codigo + ".png"));
+            BufferedImage buffered = (BufferedImage) imageReal;
+            imagem = buffered;
+
+        } catch (IOException ex) {
+            System.out.println(ex);
+            Logger.getLogger(Leica.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(3);
+        }
+
+        try {
+            geraSaida(saida, buffer);
+        } catch (IOException ex) {
+            Logger.getLogger(Leica.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(4);
+        }
+
+        imprime(job);
+
+    }
+
+    private byte[] geraPNG(String path, String codigo, int comprimento, int altura) throws WriterException, IOException {
+//        System.out.println("path, codigo, \"png\":  " + path+ codigo+ "png");
+        Code128Util code = new Code128Util();
+        byte[] arrayByte = code.buildBarCode128(path, codigo, "png");
+//        System.out.println("arrayByte: " + arrayByte);
+        return arrayByte;
+//        return fos.toByteArray();
+    }
+
+    private void geraSaida(String saida, byte[] buffer) throws FileNotFoundException, IOException {
+        if (saida == null) {
+            return;
+        }
+        FileOutputStream fos = new FileOutputStream(saida);
+        fos.write(buffer);
+        fos.flush();
+        fos.close();
+    }
+
+    private PrinterJob obtemJob(String tipoimpressora, String impressora) throws PrinterException {
+        if (impressora == null) {
+            return null;
+        }
+        PrintService ps = null;
+        ps = impressoras(impressora, tipoimpressora);
+        if (ps == null) {
+            throw new RuntimeException("Impressora e tipo não encontrados!!" + impressora + " " + tipoimpressora);
+        }
+
+        PrinterJob job = PrinterJob.getPrinterJob();
+        job.setPrintable(this);
+
+        job.setPrintService(ps);
+        job.setCopies(1);
+        return job;
+    }
+
+    private void imprime(PrinterJob job) {
+        PrintRequestAttributeSet aset = new HashPrintRequestAttributeSet();
+        MediaPrintableArea mpa = new MediaPrintableArea((float) 0.0, (float) 0.0, (float) 28.2, (float) 7.0, MediaPrintableArea.MM);
+        aset.add(mpa);
+        try {
+            job.print(aset);
+        } catch (Exception e) {
+            System.err.println("erro no job.print()");
+            e.printStackTrace();
+        }
+        /*
+         boolean doPrint = job.printDialog();
+         if (doPrint) {
+         try {
+         job.print();
+         } catch (Exception e) {
+         e.printStackTrace();
+         }
+         }
+         */
+    }
+
+    @Override
+    public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
+        if (pageIndex > 0) {
+            return NO_SUCH_PAGE;
+        }
+
+        ((Graphics2D) graphics).translate(this.tc, this.ta);
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, fc, fa);
+        if (this.imagem == null) {
+            System.out.println("this.imagem == null");
+        }
+        graphics.drawImage(this.imagem, 0, 0, fc, fa, 0, 0, this.imagem.getWidth(), this.imagem.getHeight(), null);
+        return PAGE_EXISTS;
+    }
+
+    private static PrintService impressoras(String nome, String tipoimpressora) {
+        PrintRequestAttributeSet pras = new HashPrintRequestAttributeSet();
+        pras.add(new Copies(1));
+        PrintService pss[] = null;
+        if (tipoimpressora == null) {
+            pss = PrintServiceLookup.lookupPrintServices(null, pras);
+        } else {
+            switch (tipoimpressora) {
+                case "gif":
+                    pss = PrintServiceLookup.lookupPrintServices(DocFlavor.INPUT_STREAM.GIF, pras);
+                    break;
+                case "png":
+                    pss = PrintServiceLookup.lookupPrintServices(DocFlavor.INPUT_STREAM.PNG, pras);
+                    break;
+                case "jpeg":
+                    pss = PrintServiceLookup.lookupPrintServices(DocFlavor.INPUT_STREAM.JPEG, pras);
+                    break;
+            }
+        }
+        if (pss == null || pss.length == 0) {
+            throw new RuntimeException("Sem serviço de impressao!!");
+        }
+        PrintService ps = null;
+        for (PrintService tps : pss) {
+            if (nome == null) {
+                System.out.println("Impressora " + tps.getName() + " disponivel");
+            } else {
+                if (tps.getName().equals(nome)) {
+                    ps = tps;
+                }
+            }
+        }
+        return ps;
+    }
+}
---
+++ src/main/java/br/com/img/generator/Code128Util.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.img.generator;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import static java.awt.print.Printable.NO_SUCH_PAGE;
+import static java.awt.print.Printable.PAGE_EXISTS;
+import java.awt.print.PrinterException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.imageio.ImageIO;
+import org.krysalis.barcode4j.impl.code128.Code128Bean;
+import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
+import org.krysalis.barcode4j.tools.UnitConv;
+
+/**
+ *
+ * @author eros
+ */
+public class Code128Util {
+
+    public byte[] buildBarCode128(String path, String code, String format) throws IOException {
+
+            try {
+                Code128Bean bean = new Code128Bean();
+                final int dpi = 80;
+                //Configure the barcode generator
+                bean.setModuleWidth(UnitConv.in2mm(1.0f / dpi)); //makes the narrow bar 
+                //width exactly one pixel
+                bean.setHeight(9d);
+//            bean.setWideFactor(3);
+                bean.doQuietZone(true);
+                //Open output file
+//                System.out.println("path + code + \".\" + format  :"+path + code + "." + format);
+                File outputFile = new File(path + code + "." + format);
+                OutputStream out = new FileOutputStream(outputFile);
+                try {
+                    //Set up the canvas provider for monochrome JPEG output 
+                    BitmapCanvasProvider canvas = new BitmapCanvasProvider(out, "image/" + format, dpi, BufferedImage.TYPE_BYTE_BINARY, false, 0);
+                    //Generate the barcode
+                    bean.generateBarcode(canvas, code);
+                    //Signal end of generation
+                    canvas.finish();
+                } finally {
+                    out.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            File imgPath = new File(path + code + "." + format);
+            BufferedImage bufferedImage = ImageIO.read(imgPath);
+            // get DataBufferBytes from Raster
+            WritableRaster raster = bufferedImage.getRaster();
+            DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
+            byte[] arrayReturn = (data.getData());
+
+            return arrayReturn;
+    }
+
+
+
+}
